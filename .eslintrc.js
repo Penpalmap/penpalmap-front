bfe418b9d448faf0e0a2cb35556fb78ece328355
Noiret Colin <colin.noiret@gmail.com>
2023-04-27 19:34:06 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "next",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  rules: {
+    // Ajoutez vos règles personnalisées ici
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
