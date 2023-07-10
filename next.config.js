b4fb63b1e2aa0121686b6d99d7aae8d0595e6210
Noiret Colin <colin.noiret@gmail.com>
2023-07-10 15:15:30 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     poweredByHeader: false, // Désactive l'en-tête "X-Powered-By" de Next.js pour des raisons de sécurité.
+    output: 'standalone',
 }
 
 module.exports = nextConfig
