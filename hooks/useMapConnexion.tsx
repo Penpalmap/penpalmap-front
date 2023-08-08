71e389bbe4d05977d641e9ae7f267f8c2ea89636
Noiret Colin <colin.noiret@gmail.com>
2023-08-08 18:58:57 +0200

    fictives people on map connexion

--- hooks/useMapConnexion.tsx
+++ hooks/useMapConnexion.tsx
@@ -2,7 +2,16 @@ import { useEffect, useRef } from 'react'
 import View from 'ol/View'
 import TileLayer from 'ol/layer/Tile'
 import OSM from 'ol/source/OSM'
-import { Map as OLMap } from 'ol'
+import { Feature, Map as OLMap } from 'ol'
+import fictives from '../data/fictives.json'
+import { Icon } from '@chakra-ui/react'
+import VectorLayer from 'ol/layer/Vector'
+import { fromLonLat } from 'ol/proj'
+import VectorSource from 'ol/source/Vector'
+import Style from 'ol/style/Style'
+import Photo from './../styles/openlayer/Photo'
+import { Point } from 'ol/geom'
+import Stroke from 'ol/style/Stroke'
 
 interface UseMapOptions {
     center: [number, number]
@@ -34,6 +43,36 @@ const useMapConnexion = ({}: UseMapOptions): UseMapConnexionResult => {
                 zoom: 2,
             }),
             interactions: [],
+        })
+
+        fictives.forEach((user) => {
+            const userFeature = new Feature({
+                geometry: new Point(
+                    fromLonLat([user.longitude, user.latitude])
+                ),
+            })
+
+            userFeature.setStyle(
+                new Style({
+                    image: new Photo({
+                        src: user.image,
+                        radius: 30,
+                        crop: true,
+                        kind: 'circle',
+                        shadow: 5,
+                        stroke: new Stroke({
+                            color: '#fff',
+                            width: 3,
+                        }),
+                    }),
+                })
+            )
+
+            map.addLayer(
+                new VectorLayer({
+                    source: new VectorSource({ features: [userFeature] }),
+                })
+            )
         })
 
         return () => {
