6c6ad80be25b756f6f674926a0f6dfd1cf9c66cb
gabriel.noiret <gabriel.noiret@gmail.com>
2023-10-27 17:30:53 +0200

    ajout style genre

--- components/CreateProfile/ProfileGenderInput.tsx
+++ components/CreateProfile/ProfileGenderInput.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Text, VStack } from '@chakra-ui/react'
+import { Flex, HStack, Text, VStack, Wrap, Box } from '@chakra-ui/react'
 import { UseFormSetValue } from 'react-hook-form'
 import { ProfileFormData } from '../../types'
 import { ReactElement, useState, useCallback } from 'react'
@@ -13,6 +13,9 @@ import {
     faMarsStrokeUp,
     faNeuter,
     faMercury,
+    faQuestionCircle,
+    faVenusDouble,
+    faMarsDouble,
 } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {
@@ -57,13 +60,16 @@ const ProfileGenderInput = (props: Props) => {
     const handleClick = useCallback(
         (gender: string) => {
             setValue('gender', gender)
+            if (gender === 'man' || gender === 'woman') {
+                setShowOtherGenders(false)
+            }
         },
         [setValue]
     )
 
     return (
-        <VStack spacing={4}>
-            <HStack>
+        <VStack spacing={4} overflowX="hidden" maxWidth="100%">
+            <Wrap spacing={4}>
                 <ProfileGenderInputBox
                     isSelected={selectedGender === 'woman'}
                     icon={<FontAwesomeIcon icon={faVenus} size="2x" />}
@@ -77,72 +83,169 @@ const ProfileGenderInput = (props: Props) => {
                     text="Man"
                 />
                 <ProfileGenderInputBox
-                    isSelected={selectedGender === 'other' || showOtherGenders}
+                    isSelected={selectedGender === 'other' && showOtherGenders}
                     icon={<FontAwesomeIcon icon={faVenusMars} size="2x" />}
                     onClick={() => setShowOtherGenders(!showOtherGenders)}
-                    text="Other"
+                    text="Other ..."
                 />
-            </HStack>
-
+            </Wrap>
             {showOtherGenders && (
-                <HStack>
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'transgender'}
-                        icon={
-                            <FontAwesomeIcon icon={faTransgender} size="2x" />
-                        }
-                        onClick={() => handleClick('transgender')}
-                        text="Transgender"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'genderless'}
-                        icon={<FontAwesomeIcon icon={faGenderless} size="2x" />}
-                        onClick={() => handleClick('genderless')}
-                        text="Genderless"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'genderless'}
-                        icon={<FontAwesomeIcon icon={faGenderless} size="2x" />}
-                        onClick={() => handleClick('genderless')}
-                        text="Genderless"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'bigender'}
-                        icon={
-                            <FontAwesomeIcon icon={faMarsAndVenus} size="2x" />
-                        }
-                        onClick={() => handleClick('bigender')}
-                        text="Bigender"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'androgyne'}
-                        icon={
-                            <FontAwesomeIcon icon={faMarsStrokeUp} size="2x" />
-                        }
-                        onClick={() => handleClick('androgyne')}
-                        text="Androgyne"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'neuter'}
-                        icon={<FontAwesomeIcon icon={faNeuter} size="2x" />}
-                        onClick={() => handleClick('neuter')}
-                        text="Neuter"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={selectedGender === 'non-binary'}
-                        icon={<FontAwesomeIcon icon={faMercury} size="2x" />}
-                        onClick={() => handleClick('non-binary')}
-                        text="Non-binary"
-                    />
-                    <ProfileGenderInputBox
-                        isSelected={
-                            selectedGender === 'othergender' || showOtherGenders
-                        }
-                        icon={<FontAwesomeIcon icon={faVenusMars} size="2x" />}
-                        onClick={() => handleClick('othergender')}
-                        text="Other Gender"
-                    />
-                </HStack>
+                <Box
+                    maxHeight="200px"
+                    overflowY="auto"
+                    width="100%"
+                    sx={{
+                        '&::-webkit-scrollbar': {
+                            width: '5px',
+                        },
+                        '&::-webkit-scrollbar-track': {
+                            boxShadow: 'inset 0 0 5px grey',
+                            borderRadius: '4px',
+                        },
+                        '&::-webkit-scrollbar-thumb': {
+                            backgroundColor: 'darkgrey',
+                            borderRadius: '4px',
+                        },
+                        '&::-webkit-scrollbar-thumb:hover': {
+                            backgroundColor: 'grey',
+                        },
+                    }}
+                >
+                    <Wrap spacing={4} maxWidth="640px">
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'transgender'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faTransgender}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('transgender')}
+                            text="Transgender"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'genderless'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faGenderless}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('genderless')}
+                            text="Genderless"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'bigender'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faMarsAndVenus}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('bigender')}
+                            text="Bigender"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'androgyne'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faMarsStrokeUp}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('androgyne')}
+                            text="Androgyne"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'neuter'}
+                            icon={<FontAwesomeIcon icon={faNeuter} size="2x" />}
+                            onClick={() => handleClick('neuter')}
+                            text="Neuter"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'non-binary'}
+                            icon={
+                                <FontAwesomeIcon icon={faMercury} size="2x" />
+                            }
+                            onClick={() => handleClick('non-binary')}
+                            text="Non-binary"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'genderqueer'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faMarsAndVenus}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('genderqueer')}
+                            text="Genderqueer"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'genderfluid'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faTransgender}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('genderfluid')}
+                            text="Genderfluid"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'two-spirit'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faMarsDouble}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('two-spirit')}
+                            text="Two-Spirit"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'agender'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faGenderless}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('agender')}
+                            text="Agender"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'pangender'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faVenusDouble}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('pangender')}
+                            text="Pangender"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'third-gender'}
+                            icon={
+                                <FontAwesomeIcon icon={faVenusMars} size="2x" />
+                            }
+                            onClick={() => handleClick('third-gender')}
+                            text="Third Gender"
+                        />
+                        <ProfileGenderInputBox
+                            isSelected={selectedGender === 'othergender'}
+                            icon={
+                                <FontAwesomeIcon
+                                    icon={faQuestionCircle}
+                                    size="2x"
+                                />
+                            }
+                            onClick={() => handleClick('othergender')}
+                            text="Other"
+                        />
+                    </Wrap>
+                </Box>
             )}
         </VStack>
     )
