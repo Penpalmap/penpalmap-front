dd45bb3a52119c39359e4b3814e8b9ff89407d66
gabriel.noiret <gabriel.noiret@gmail.com>
2023-10-30 14:03:40 +0100

    ajout zoom

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,33 +1,78 @@
 import { Box } from '@chakra-ui/react'
 import 'ol/ol.css'
+import * as ol from 'ol'
+import { fromLonLat } from 'ol/proj'
 import useMap from '../../hooks/useMap'
 import OverlayProfileMap from './OverlayProfileMap'
 import { useCallback, useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
+import CitySearchInput from '../CreateProfile/CitySearchInput'
+
+type MapRefObject = {
+    current: ol.Map | null
+}
 
 const Map = () => {
     const { mapContainerRef, mapObj, overlayRef } = useMap({
         center: [0, 0],
         zoom: 2,
-    })
+    }) as {
+        mapContainerRef: React.RefObject<HTMLDivElement>
+        mapObj: MapRefObject
+        overlayRef: any
+    }
 
     const [data, setData] = useContext(AppContext)
 
     const closeOverlay = useCallback(() => {
-        if (!mapObj.current || !overlayRef) return
-        overlayRef.setPosition(undefined)
+        if (mapObj.current && overlayRef) {
+            overlayRef.setPosition(undefined)
+        }
     }, [mapObj, overlayRef])
+
+    const handleLocationSelected = (lat, lon, displayName) => {
+        if (!mapObj.current) return
+
+        const view = mapObj.current.getView()
+        const currentCenter = view.getCenter() as [number, number] | undefined
+        const currentZoom = view.getZoom()
+
+        if (!currentCenter || currentZoom === undefined) return
+
+        const numericLat = parseFloat(lat)
+        const numericLon = parseFloat(lon)
+        const destination = fromLonLat([numericLon, numericLat])
+
+        const distance = Math.sqrt(
+            Math.pow(currentCenter[0] - destination[0], 2) +
+                Math.pow(currentCenter[1] - destination[1], 2)
+        )
+
+        const intermediateZoom =
+            distance < 1000000
+                ? currentZoom
+                : distance < 6000000
+                ? currentZoom - 1
+                : currentZoom - 4
+
+        view.animate(
+            { zoom: intermediateZoom, duration: 700 },
+            { center: destination, duration: 1000 },
+            { zoom: 10, duration: 700 }
+        )
+    }
 
     return (
         <>
+            <CitySearchInput onLocationSelected={handleLocationSelected} />
             <Box
                 ref={mapContainerRef}
-                h={'calc(100vh - 56px)'}
-                w={'full'}
+                h="calc(100vh - 56px)"
+                w="full"
                 className="map"
-                bg="#8CBBD4" // Ajout de la couleur de fond
-            ></Box>
-            <Box display={'none'}>
+                bg="#8CBBD4"
+            />
+            <Box display="none">
                 <OverlayProfileMap
                     user={data.userTarget}
                     closeOverlay={closeOverlay}
@@ -43,4 +88,5 @@ const Map = () => {
         </>
     )
 }
+
 export default Map
