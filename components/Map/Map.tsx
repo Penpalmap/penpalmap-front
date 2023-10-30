48ba1ceca61dd9586550bb274fe4184cf9f71789
gabriel.noiret <gabriel.noiret@gmail.com>
2023-10-30 14:15:04 +0100

    ajout component SearchAndZoom

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,12 +1,11 @@
 import { Box } from '@chakra-ui/react'
 import 'ol/ol.css'
 import * as ol from 'ol'
-import { fromLonLat } from 'ol/proj'
 import useMap from '../../hooks/useMap'
 import OverlayProfileMap from './OverlayProfileMap'
 import { useCallback, useContext } from 'react'
 import { AppContext } from '../../context/AppContext'
-import CitySearchInput from '../CreateProfile/CitySearchInput'
+import SearchAndZoom from './SearchAndZoom'
 
 type MapRefObject = {
     current: ol.Map | null
@@ -30,41 +29,9 @@ const Map = () => {
         }
     }, [mapObj, overlayRef])
 
-    const handleLocationSelected = (lat, lon, displayName) => {
-        if (!mapObj.current) return
-
-        const view = mapObj.current.getView()
-        const currentCenter = view.getCenter() as [number, number] | undefined
-        const currentZoom = view.getZoom()
-
-        if (!currentCenter || currentZoom === undefined) return
-
-        const numericLat = parseFloat(lat)
-        const numericLon = parseFloat(lon)
-        const destination = fromLonLat([numericLon, numericLat])
-
-        const distance = Math.sqrt(
-            Math.pow(currentCenter[0] - destination[0], 2) +
-                Math.pow(currentCenter[1] - destination[1], 2)
-        )
-
-        const intermediateZoom =
-            distance < 1000000
-                ? currentZoom
-                : distance < 6000000
-                ? currentZoom - 1
-                : currentZoom - 4
-
-        view.animate(
-            { zoom: intermediateZoom, duration: 700 },
-            { center: destination, duration: 1000 },
-            { zoom: 10, duration: 700 }
-        )
-    }
-
     return (
         <>
-            <CitySearchInput onLocationSelected={handleLocationSelected} />
+            <SearchAndZoom mapObj={mapObj} />
             <Box
                 ref={mapContainerRef}
                 h="calc(100vh - 56px)"
