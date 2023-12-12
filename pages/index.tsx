9e717224f9821ab5d86e79c35403c1fd1fb2fdc0
Noiret Colin <colin.noiret@gmail.com>
2023-12-12 19:59:02 +0100

    Add token expiration check in Home component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,14 +7,27 @@ import Modal from 'react-modal'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import LayoutMobile from '../components/Layout/LayoutMobile'
 import LayoutDesktop from '../components/Layout/LayoutDesktop'
+import { jwtDecode } from 'jwt-decode'
 Modal.setAppElement('#__next')
 
 export default function Home() {
     const { session, status } = useSession()
 
     const isMobile = useBreakpointValue({ base: true, md: false })
-
+    const checkTokenExpiration = () => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            const decoded = jwtDecode(token)
+            if (decoded.exp) {
+                if (decoded.exp < Date.now() / 1000) {
+                    localStorage.removeItem('token')
+                    Router.push('/auth/signin')
+                }
+            }
+        }
+    }
     useEffect(() => {
+        checkTokenExpiration()
         if (session?.user?.isNewUser) {
             Router.push('/create-profile')
         }
