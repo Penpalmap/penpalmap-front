3fb33b2cfc5b7c665df2ceef4bde1f9c924a6d7e
gabriel.noiret <gabriel.noiret@gmail.com>
2024-01-15 11:36:51 +0100

    Add FontAwesome icons to footer links

--- components/Layout/footer.tsx
+++ components/Layout/footer.tsx
@@ -1,7 +1,13 @@
-// Footer.js
 import React from 'react'
 import { Box, Flex, HStack, Image, Link, Text, VStack } from '@chakra-ui/react'
 import { useTranslation } from 'next-i18next'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faInfoCircle,
+    faEnvelope,
+    faFileAlt,
+    faBalanceScale,
+} from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
     const { t } = useTranslation()
@@ -22,29 +28,45 @@ const Footer = () => {
                     </VStack>
                     <Flex alignItems={'flex-start'} gap={'10'}>
                         {/* Column 1: Company Info */}
-                        <VStack spacing="2" align="center">
+                        <VStack spacing="2" align="flex-start">
+                            <Text fontWeight="bold">Company Info</Text>
                             <Link
                                 href="/about"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
+                                <FontAwesomeIcon
+                                    icon={faInfoCircle}
+                                    style={{ marginRight: '5px' }}
+                                />
                                 {t('footer.about')}
                             </Link>
                             <Link
                                 href="/contact"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
+                                <FontAwesomeIcon
+                                    icon={faEnvelope}
+                                    style={{ marginRight: '5px' }}
+                                />
                                 {t('footer.contact')}
                             </Link>
                         </VStack>
 
                         {/* Column 2: Social Media Links */}
-                        <VStack spacing="2" align="center">
+                        <VStack spacing="2" align="flex-start">
+                            <Text fontWeight="bold">Social Media</Text>
                             <Link
                                 href="https://www.instagram.com/penpalmap"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
                                 Instagram
                             </Link>
@@ -52,6 +74,8 @@ const Footer = () => {
                                 href="https://www.facebook.com/penpalmap"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
                                 Facebook
                             </Link>
@@ -59,25 +83,40 @@ const Footer = () => {
                                 href="https://www.linkedin.com/company/penpalmap"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
                                 LinkedIn
                             </Link>
                         </VStack>
 
                         {/* Column 3: Legal Links */}
-                        <VStack spacing="2" align="center">
+                        <VStack spacing="2" align="flex-start">
+                            <Text fontWeight="bold">Legal Links</Text>
                             <Link
                                 href="/terms"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
+                                <FontAwesomeIcon
+                                    icon={faFileAlt}
+                                    style={{ marginRight: '5px' }}
+                                />
                                 {t('footer.terms')}
                             </Link>
                             <Link
                                 href="/legalnotice"
                                 color="blue.600"
                                 fontWeight="medium"
+                                display="flex"
+                                alignItems="center"
                             >
+                                <FontAwesomeIcon
+                                    icon={faBalanceScale}
+                                    style={{ marginRight: '5px' }}
+                                />
                                 {t('footer.legal')}
                             </Link>
                         </VStack>
