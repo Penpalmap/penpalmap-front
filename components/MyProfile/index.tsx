be331a6ea88d29e50d1d3dcb1b6b7c0d002e2914
Colingue <colin.noiret@gmail.com>
2024-02-03 17:40:53 +0100

    clean routes (update bio useless)

--- components/MyProfile/index.tsx
+++ components/MyProfile/index.tsx
@@ -16,7 +16,7 @@ import { UserImage } from '../../types'
 import ProfileImage from '../Profile/ProfileImages'
 import { useTranslation } from 'next-i18next'
 import { useSession } from '../../hooks/useSession'
-import { updateBio, updateUser } from '../../api/userApi'
+import { updateUser } from '../../api/userApi'
 import MapInput from '../Elements/form/mapInput'
 
 type Props = {
