603c4251108f0de670cb7886623678c0c2d147ad
Colingue <colin.noiret@gmail.com>
2024-02-11 20:17:25 +0100

    add number to count total unread message

--- components/Layout/LayoutDesktop.tsx
+++ components/Layout/LayoutDesktop.tsx
@@ -3,12 +3,18 @@ import Chat from '../Chat/Chat'
 import Head from 'next/head'
 import Map from '../Map/Map'
 import ConversationList from '../Conversation/ConversationList'
+import { useRoom } from '../../context/RoomsContext'
 
 const LayoutDesktop = () => {
+  const { totalUnreadMessagesNumber } = useRoom()
   return (
     <>
       <Head>
-        <title>PenpalMap</title>
+        <title>
+          {totalUnreadMessagesNumber === 0
+            ? `PenpalMap`
+            : `(${totalUnreadMessagesNumber}) PenpalMap`}
+        </title>
       </Head>
       <Box w={'full'} h={'full'} display={'flex'} position={'relative'}>
         <Box>
