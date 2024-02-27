22064eb2d1d825c49ad5de881d82e2e6ad24ce12
gabriel.noiret <gabriel.noiret@gmail.com>
2024-02-27 14:36:54 +0100

    Refactor scroll functionality in WhereIsPart component

--- components/LandingPage/whereIsPart.tsx
+++ components/LandingPage/whereIsPart.tsx
@@ -1,10 +1,4 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react'
-import { useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faChevronLeft,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons'
 
 const CityBlock = ({
   image,
@@ -41,78 +35,57 @@ const CityBlock = ({
 }
 
 const WhereIsPart = () => {
-  const cities = [
-    { image: '/images/lp/usa.jpg', cityName: 'USA' },
-    { image: '/images/lp/canada.jpg', cityName: 'Canada' },
-    { image: '/images/lp/france.jpg', cityName: 'France' },
-    { image: '/images/lp/united_kingdom.jpg', cityName: 'United Kingdom' },
-    { image: '/images/lp/brasil.jpg', cityName: 'Brasil' },
-    { image: '/images/lp/danemark.jpg', cityName: 'Danemark' },
-    { image: '/images/lp/australia.jpg', cityName: 'Australia' },
-    { image: '/images/lp/thailand.jpg', cityName: 'Thailand' },
-  ]
-
-  const [currentIndex, setCurrentIndex] = useState(0)
-
   const scrollRight = () => {
-    if (currentIndex < cities.length - 1) {
-      setCurrentIndex(currentIndex + 1)
+    const container = document.getElementById('cityContainer')
+    if (container) {
+      container.scrollBy({ left: 200, behavior: 'smooth' })
     }
   }
 
   const scrollLeft = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1)
+    const container = document.getElementById('cityContainer')
+    if (container) {
+      container.scrollBy({ left: -200, behavior: 'smooth' })
     }
   }
 
   return (
-    <Flex
-      position="relative"
-      overflow="hidden"
-      mx={-4}
-      py={6}
-      mb={20}
-      alignItems="center"
-    >
-      {currentIndex > 0 && (
-        <Box
-          position="absolute"
-          top="50%"
-          left="0"
-          transform="translateY(-50%)"
-          cursor="pointer"
-          onClick={scrollLeft}
-          zIndex={1}
-          px={2}
-        >
-          <FontAwesomeIcon icon={faChevronLeft} size="2x" color="white" />
-        </Box>
-      )}
-      <Flex
-        id="cityContainer"
-        transform={`translateX(-${currentIndex * 100}%)`}
-        transition="transform 0.5s ease"
-        alignItems="center"
+    <Flex overflow="hidden" mx={-4} py={6} position="relative">
+      <Box id="cityContainer" display="flex" mx={4} overflowX="auto">
+        <CityBlock image={'/images/lp/usa.jpg'} cityName={'USA'} />
+        <CityBlock image={'/images/lp/canada.jpg'} cityName={'Canada'} />
+        <CityBlock image={'/images/lp/france.jpg'} cityName={'France'} />
+        <CityBlock
+          image={'/images/lp/united_kingdom.jpg'}
+          cityName={'United Kingdom'}
+        />
+        <CityBlock image={'/images/lp/brasil.jpg'} cityName={'Brasil'} />
+        <CityBlock image={'/images/lp/danemark.jpg'} cityName={'Danemark'} />
+        <CityBlock image={'/images/lp/australia.jpg'} cityName={'Australia'} />
+        <CityBlock image={'/images/lp/thailand.jpg'} cityName={'Thailand'} />
+      </Box>
+      <Box
+        position="absolute"
+        top="50%"
+        left="0"
+        transform="translateY(-50%)"
+        cursor="pointer"
+        onClick={scrollLeft}
+        zIndex={1}
       >
-        {cities.map((city, index) => (
-          <CityBlock key={index} image={city.image} cityName={city.cityName} />
-        ))}
-      </Flex>
-      {currentIndex < cities.length - 1 && (
-        <Box
-          position="absolute"
-          top="50%"
-          right="0"
-          transform="translateY(-50%)"
-          cursor="pointer"
-          onClick={scrollRight}
-          zIndex={1}
-          px={2}
-        >
-          <FontAwesomeIcon icon={faChevronRight} size="2x" color="white" />
-        </Box>
-      )}
+        {'<'}
+      </Box>
+      <Box
+        position="absolute"
+        top="50%"
+        right="0"
+        transform="translateY(-50%)"
+        cursor="pointer"
+        onClick={scrollRight}
+        zIndex={1}
+      >
+        {'>'}
+      </Box>
     </Flex>
   )
 }
