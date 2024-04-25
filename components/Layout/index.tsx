debaf133875529ce19cc11ed73983d757dd88886
Colingue <colin.noiret@gmail.com>
2024-04-25 20:30:05 +0200

    RESPONSIVE: heigth home

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,29 +1,15 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Footer from './footer'
 import { useRouter } from 'next/router'
 import Header from './header'
 import { useMobileView } from '../../context/MobileViewContext'
-import { useEffect, useState } from 'react'
 
 const Layout = ({ children }) => {
   const router = useRouter()
   const { mobileView, isMobile } = useMobileView()
 
-  const [viewportHeight, setViewportHeight] = useState('100vh')
-
-  useEffect(() => {
-    const handleResize = () => {
-      setViewportHeight(`${window.innerHeight}px`)
-    }
-
-    window.addEventListener('resize', handleResize)
-    handleResize()
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
   return (
-    <Flex direction={'column'} h={viewportHeight}>
+    <>
       {router.pathname !== '/auth/signup' &&
         router.pathname !== '/auth/signin' &&
         !(isMobile && mobileView === 'chat') &&
@@ -38,7 +24,7 @@ const Layout = ({ children }) => {
         router.pathname !== '/auth/signup' &&
         router.pathname !== '/auth/signin' &&
         router.pathname !== '/create-profile' && <Footer />}
-    </Flex>
+    </>
   )
 }
 
