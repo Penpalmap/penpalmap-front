40d79192b7539650b9973c51b79f26e7737b0d87
Colingue <colin.noiret@gmail.com>
2024-04-26 19:47:28 +0200

    Add WhereIsPart component to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import TestimonialsPart from '../components/LandingPage/testimonialsPart'
 import CallToAction from '../components/LandingPage/callToAction'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { useMobileView } from '../context/MobileViewContext'
+import WhereIsPart from '../components/LandingPage/whereIsPart'
 
 export default function Home() {
   const { isMobile } = useMobileView()
